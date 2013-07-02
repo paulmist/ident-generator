c5b1dcbd0955204e698d35c82891b7f8b692fa42
Paul Mist <paulmist@gmail.com>
2013-07-02 17:11:56 +0100

    Update

--- download.php
+++ download.php
@@ -1,15 +1,11 @@
 <?php
-if($_POST['addition']){
-	$file = 'ident.svg';
-	
-	
-	header("Cache-Control: public");
+$file = 'ident.svg';
+$data = $_POST['addition'];
+header("Cache-Control: public");
 	header("Content-Description: File Transfer");
-	//header("Content-Length: ". filesize("$file").";");
+	header("Content-Length: ". filesize("$file").";");
 	header("Content-Disposition: attachment; filename=$file");
 	header("Content-Type: application/octet-stream; "); 
 	header("Content-Transfer-Encoding: binary");
-	$data = $_POST['addition'];
 	echo $data;
-}
 ?>
