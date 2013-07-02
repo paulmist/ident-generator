6ea7e7b83a3a5517bbb345541d921dd02b8741b3
Paul Mist <paulmist@gmail.com>
2013-07-02 17:09:17 +0100

    Updated download

--- download.php
+++ download.php
@@ -5,7 +5,7 @@ if($_POST['addition']){
 	
 	header("Cache-Control: public");
 	header("Content-Description: File Transfer");
-	header("Content-Length: ". filesize("$file").";");
+	//header("Content-Length: ". filesize("$file").";");
 	header("Content-Disposition: attachment; filename=$file");
 	header("Content-Type: application/octet-stream; "); 
 	header("Content-Transfer-Encoding: binary");
