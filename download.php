5a8e4453d4c2928ccc11ff95655f958c29bd7a1f
Paul Mist <paulmist@gmail.com>
2013-07-09 09:32:06 +0100

    File error fix

--- download.php
+++ download.php
@@ -1,9 +1,9 @@
 <?php
-$file = 'ident.svg';
+$file = "ident.svg";
 $data = $_POST['addition'];
-header("Cache-Control: public");
+	header("Cache-Control: public");
 	header("Content-Description: File Transfer");
-	header("Content-Length: ". filesize('$file').";");
+	header("Content-Length: ". filesize('/$file') .";");
 	header("Content-Disposition: attachment; filename=$file");
 	header("Content-Type: application/octet-stream; "); 
 	header("Content-Transfer-Encoding: binary");
